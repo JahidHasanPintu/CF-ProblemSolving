a9d6658684002aef05cfc9c35696bebc4a69e147
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-04-03 00:26:09 +0600

    uva contest

---
+++ adhocG.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    long y,x;
+    scanf("%ld",&y);
+    x=sqrt(y);
+    printf("%ld",x);
+    return 0;
+}
---
+++ adhocG.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <cmath>
+
+#include<cstdio>
+#include<math.h>
+using namespace std;
+int main(){
+    long double y;
+    scanf("%lf",&y);
+    printf("%.0lf\n",sqrtl(y));
+
+  return 0;
+}
