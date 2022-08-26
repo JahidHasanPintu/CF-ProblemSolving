6a31416f49d586b2be9c67b2e91e0cbccc31d7c9
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-08-26 22:16:26 +0600

    266A stone on table another way

--- 266A-StoneOnTable.cpp
+++ 266A-StoneOnTable.cpp
@@ -2,22 +2,39 @@
 using namespace std;
 int main()
 {
-    string s;
-    cin >> s;
-    int n = s.length();
+    // if input is a string
+    // string s;
+    // cin >> s;
+    // int n = s.length();
 
-    // declaring character array
-    char char_array[n + 1];
+    // // declaring character array
+    // char char_array[n + 1];
 
-    // copying the contents of the
-    // string to char array
-    strcpy(char_array, s.c_str());
-    int count = 0;
+    // // copying the contents of the
+    // // string to char array
+    // strcpy(char_array, s.c_str());
+    // int count = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // printing the array
+    //     // cout << char_array[i] << endl;
+    //     if (char_array[i] == char_array[i + 1])
+    //     {
+    //         count++;
+    //     }
+    // }
+    // cout << count << endl;
+    int n, i, count = 0;
+    cin >> n;
+    char arr[n];
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
     for (int i = 0; i < n; i++)
     {
-        // printing the array
-        // cout << char_array[i] << endl;
-        if (char_array[i] == char_array[i + 1])
+        if (arr[i] == arr[i + 1])
         {
             count++;
         }
