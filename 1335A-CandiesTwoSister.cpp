c1bb4bb6a81576ebc24d5685eabe7db1493a59af
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-01-14 10:36:04 +0600

    candies and two sister

---
+++ 1335A-CandiesTwoSister.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        long long n; cin>>n;
+        // int long long ans=0;
+        // for(int i=0;i<n/2;i++){
+        //     ans=
+        // }
+        if(n%2==0){
+            cout<<n/2-1<<endl;
+        }else{
+            cout<<n/2<<endl;
+        }
+        
+    }
+}
